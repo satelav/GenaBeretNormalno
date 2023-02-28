38672467370fa5efd7470b9d7694d691b8d146ab
Alex <95912651+satelav@users.noreply.github.com>
2023-02-28 10:46:42 +0300

    Add a code
    ЕЕЕЕЕ я добавил

---
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2f49a136-fef8-4555-814a-3b3f02490ebc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using ConsoleApp1;
+using Microsoft.CodeAnalysis;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int i= 1;
+            int j= 2;
+        
+
+            int actual = 0;
+
+          Boom  sol = new Boom(i,j);
+            int result = 0;
+      
+            Assert.AreEqual(actual, sol.Solution(result));
+        }
+    }
+}
